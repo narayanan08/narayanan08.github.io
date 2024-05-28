d9da9bbb49b78d77ca2641b5d44475a0e5b15178
Badri <vtbn012004@gmail.com>
2024-05-28 10:10:07 +0530

    Added requirements.txt file and removed an unused module in app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 from ultralytics import YOLO
 import os
-import shutil
 import uuid
 import cv2
 
